834c4cefb62023bb63ff019ec6e14436dccfbd2c
michalkrawczyk <mkrwczyk.1@gmail.com>
2023-06-16 20:27:07 +0200

    Finished Retriever

--- arxiv_utils/retriever.py
+++ arxiv_utils/retriever.py
@@ -5,20 +5,26 @@ from .wrapper import ArxivAPIWrapper2
 
 
 class ExtendedArxivRetriever(BaseRetriever, ArxivAPIWrapper2):
+    """
+    Wrapper for ArxivAPIWrapper2 (Extended ArxivAPIWrapper. Used without changes).
+    Wraps load() for obtaining documents with
+    get_relevant_documents(), get_documents_by_id() and get_filtered_documents_by_query()
+    """
+
     def get_relevant_documents(self, query: str) -> List[Document]:
-        """ Get relevant documents by search quarry"""
-        # TODO
-        pass
+        """Get relevant documents by search quarry"""
+        # TODO: Should be changed self.sort_docs_by?
+        return self.load(query=query)
 
-    def get_documents_by_id(self, id_list: List[int]) -> List[Document]:
-        """ Get relevant documents by document ids"""
-        # TODO
-        pass
+    def get_documents_by_id(self, id_list: List[str]) -> List[Document]:
+        """Get relevant documents by document ids"""
+        return self.load(id_list=id_list)
 
-    def filter_documents_by_query(self, id_list: List[str], query: str) -> List[Document]:
-        """ Get relevant documents by document ids matching query"""
-        # TODO
-        pass
+    def get_filtered_documents_by_query(
+        self, id_list: List[str], query: str
+    ) -> List[Document]:
+        """Get relevant documents by document ids matching query"""
+        return self.load(query=query, id_list=id_list)
 
     async def aget_relevant_documents(self, query: str) -> List[Document]:
         raise NotImplementedError
