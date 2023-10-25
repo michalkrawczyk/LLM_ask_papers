6213513c98282566033212f5507172483d114759
michalkrawczyk <mkrwczyk.1@gmail.com>
2023-10-25 21:31:43 +0200

    base test units for database

--- datasets/tests/test_langchain_dataset.py
+++ datasets/tests/test_langchain_dataset.py
@@ -1,45 +1,117 @@
 import pytest
 import os
+import yaml
 from pathlib import Path
-# TODO: test all add functions
-# TODO: test llm search?
 
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
+
+#TODO: embeddings and dataset same for all test units?
 
 from datasets import PaperDatasetLC
 ROOT_PATH = Path(__file__).resolve().parents[2]
 
-# def test_sample_files_exist():
-#     """ Check if needed samples for dir exists"""
-#     assert os.path.isdir(ROOT_PATH / "sample_documents"), "No Sample Dir"
-#     assert os.path.isfile(ROOT_PATH / "sample_documents/2302.00386.pdf")
+try:
+    import openai
+    from langchain.embeddings import OpenAIEmbeddings
+
+    with open(ROOT_PATH / "openai_key.yaml", "r") as f:
+        openai.api_key = yaml.safe_load(f)["openai_api_key"]
+        OPENAI_AVAILABLE = True
+
+except Exception as err:
+    OPENAI_AVAILABLE = False
+    print(err)
+
+try:
+    from langchain.embeddings import HuggingFaceEmbeddings
+    import sentence_transformers
+
+    SENTENCE_TRANSFORMERS_AVAILABLE = True
+
+except ImportError:
+    SENTENCE_TRANSFORMERS_AVAILABLE = False
 
 
-def test_add_documents():
-    embeddings = HuggingFaceEmbeddings()
+@pytest.mark.skipif(not (SENTENCE_TRANSFORMERS_AVAILABLE and OPENAI_AVAILABLE), reason="Not found any llm module")
+def test_document_storage():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L12-v2") \
+        if SENTENCE_TRANSFORMERS_AVAILABLE else (
+        OpenAIEmbeddings(openai_api_key=openai.api_key, model="text-embedding-ada-002"))
+
     dataset = PaperDatasetLC(db=Chroma(embedding_function=embeddings))
+    # add documents
     dataset.add_pdf_file(str(ROOT_PATH / "sample_documents/2302.00386.pdf"))
-
     sample_text = ["Lorem impsum something something", "Some Text"]
-    sample_metas = [{"source": "sth"}, {"other": "sth"}]
-    dataset.add_texts(sample_text, sample_metas, skip_invalid=True)
+    sample_metas = [{"source": "sth", "v": "test"}, {"other": "sth"}]
+    # ^ second one should be ignored due to not specified source
+    assert len(
+        dataset.add_texts(sample_text, sample_metas, skip_invalid=True)) == 1  # only one text should be added
 
-    stored_docs = dataset.list_papers_by_uuid()
-    assert len(stored_docs) == 6    # 5 from pdf + one text
-    assert any(name == "sth" for _, name in stored_docs)
+    # Listing documents
+    stored_docs = dataset.list_documents_by_id()
+
+    assert len(stored_docs) == 6, "Invalid length from 'list_documents_by_id'"  # 5 from pdf + one text
+    print(dataset.unique_list_of_documents())
+
+    assert len(dataset.unique_list_of_documents()) == 2, \
+        "'unique_list_of_documents' should be limited to 2 (pdf + text)"
+
+    assert dataset.get_by_id(stored_docs[0][0], include=["metadatas"])["metadatas"], \
+        "get_by_id failed"
+
+    print("Found not empty", dataset.get(where={"source": "sth"}, include=['documents']))
+
+    assert dataset.get_containing_field("file_path",  include=["metadatas"])["metadatas"], \
+        "file_path should be in metadata for pdf files"
+
+    # Searching
+    assert dataset.search_by_name('2302.00386.pdf', regex_match=True, include=["metadatas"])["metadatas"],\
+        "search_by_name should contain found data"
+
+    assert len(
+        dataset.similarity_search("new feature", n_results=2)) == 2, "similarity_search should return 2 results"
 
 
-#TODO:
-# def test_search():
-#     dataset = PaperDatasetLC(db=Chroma(embedding_function=HuggingFaceEmbeddings()))
-#     doc1 = dataset.add_papers_by_id(["2301.05586"])
-#     assert len(doc1) > 0
-#     doc2 = dataset.add_documents_by_query(query="yolov7", max_docs=1)
-#
-#     assert len(doc2) > 0
-#     docs = dataset.similarity_search("Results", n_results=2)
-#
-#     assert len(docs) == 2
+@pytest.mark.skipif(not OPENAI_AVAILABLE, reason="OpenAI not available (not installed or invalid key)")
+def test_llm():
+    from langchain.prompts import PromptTemplate
+    from langchain.chat_models import ChatOpenAI
+    from templates import DEFAULT_PROMPT_REGISTER, create_and_register_prompt
+
+    model_name = "gpt-3.5-turbo"
+
+    text = """Create short, specific summary for research paper. Identify the following items for given text:
+  - Model Name
+  - Model category(e.g Object Detection, NLP or image generation)
+  - SOTA: if Model is State-of-the-Art
+  - New Features: Introduced model components, layers or other features, as keywords
+  - New Strategies: New introduced learning strategies
+  - Year: Year of publishing
+
+  text: {text}
+
+  {format_instructions}
+  """
+    create_and_register_prompt(name="identify_features", template=text, input_variables=["text", "format_instructions"])
+
+    prompt = PromptTemplate(template=text, input_variables=["text", "format_instructions"])
+
+    DEFAULT_PROMPT_REGISTER.load_defined_prompt(name="identify_features", prompt=prompt)
+
+    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key, model="text-embedding-ada-002")
+    model = ChatOpenAI(openai_api_key=openai.api_key, model=model_name)
+    # chain = LLMChain(llm=model, prompt=prompt)
+
+    dataset = PaperDatasetLC(db=Chroma(embedding_function=embeddings), llm=model)
+
+    docs_ids = dataset.add_pdf_file(str(ROOT_PATH / "sample_documents/2302.00386.pdf"))
+
+    print("llm search")
+    # print(dataset.llm_search_with_sources("identify_features"))
+    dataset.update_document_features(docs_ids[0])
+    print(dataset.get_containing_field("new_features", include=["metadatas"]))
+
+
+
 
 
