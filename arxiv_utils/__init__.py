54c19706c711a99e0da2e3ea934d2523bdb71c09
michalkrawczyk <mkrwczyk.1@gmail.com>
2023-11-30 20:35:34 +0100

    Removed unnecessary PaperData class

--- arxiv_utils/__init__.py
+++ arxiv_utils/__init__.py
@@ -1,6 +1,5 @@
 # TODO: delete download_papers and usages
 from .download_papers import download_paper_from_arxiv, download_recent_papers_by_querry
-from .containers import PageData, PaperData
 
 from .retriever import ExtendedArxivRetriever
 from .wrapper import ArxivAPIWrapper2
@@ -12,8 +11,6 @@ __all__ = [
     "download_recent_papers_by_querry",
     "ExtendedArxivRetriever",
     "ArxivAPIWrapper2",
-    "PageData",
-    "PaperData",
     "SortOrder",
     "SortCriterion",
 ]
