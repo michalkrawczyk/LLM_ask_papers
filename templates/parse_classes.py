27c4a40579c01e786f8125fbbe68c298c27e2a24
michalkrawczyk <mkrwczyk.1@gmail.com>
2024-02-22 23:24:07 +0100

    ShortInfoSummary fixed types

--- templates/parse_classes.py
+++ templates/parse_classes.py
@@ -1,23 +1,22 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Union
 
 # Note: description in pydentic classes is included in output parser
 
 
 class ShortInfoSummary(BaseModel):
     model_name: str = Field(
-        description="Name of the model if provided", default="Unknown"
+        description="Name of the model if provided"
     )
     model_category: str = Field(
         description="Model Category (e.g Object Detection, NLP or image generation)",
-        default="Unknown",
+
     )
-    sota: bool = Field(description="Is this model State-of-the-Art?", default=False)
-    new_features: List[str] = Field(
-        description="Introduced model components, layers or other features, as keywords",
-        default=[],
+    sota: int = Field(description="Boolean - Is this model State-of-the-Art?")
+    new_features: Union[List[str], str] = Field(
+        description="Introduced new model components, layers or other features, as keywords, each seperated by commas",
     )
-    new_strategies: List[str] = Field(
-        description="New strategies introduced, as keywords, separated by commas", default=[]
+    new_strategies: Union[List[str], str]  = Field(
+        description="New strategies introduced, as keywords"
     )
-    date: str = Field(description="Date of the paper", default="Unknown")
+    date: str = Field(description="Date of the paper")
